29e19e21938f1019c37ce732ebcfd2a06b73f773
Vaishali Gupta <vaishaligupta0205@gmail.com>
2020-01-10 20:32:55 -0600

    First React Commit

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle ,Breadcrumb ,BreadcrumbItem} from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+
+function RenderCampsite({campsite}){
+        return(
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
+
+function RenderComments({comments}){
+        if(comments){
+            return(
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                        {comments.map(n => <div key={n.id}> <p> <div>{n.text}</div> <div>--- {n.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(n.date)))}</div> </p> </div>)}
+                </div>
+            );
+        }else{
+            return(<div></div>);
+        }
+}
+
+
+function CampsiteInfo(props){
+        if(props.campsite){
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <Breadcrumb>
+                                <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                                 <BreadcrumbItem active> {props.campsite.name} </BreadcrumbItem>
+                            </Breadcrumb>
+                            <h2>{props.campsite.name}</h2>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <RenderCampsite campsite={props.campsite}/>
+                        <RenderComments comments={props.comments}/>
+                     </div> 
+                </div>
+            );
+        }else{
+            return <div></div> ;
+        }
+    }
+
+export default CampsiteInfo;
